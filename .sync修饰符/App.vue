8c0d1a2e736c0ba44ec17e2daa318b205dc36a9b
linya <cdthanye32@163.com>
2021-09-26 22:53:34 +0800

    ✨ feat(.sync 修饰符): 测试 .sync 修饰符的双向绑定功能

---
+++ .sync修饰符/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <sync :count.sync="data"></sync>
+    app:data= {{data}}
+    <button @click="data++">plus</button>
+  </div>
+</template>
+
+<script>
+import sync from "./sync.vue";
+export default {
+  components: {
+    sync,
+  },
+  data() {
+    return {
+      data: 0,
+    };
+  },
+};
+</script>
+
+
---
+++ .sync修饰符/sync.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="sync">
+        sync:count = {{count}}
+    </div>
+</template>
+
+<script>
+export default {
+  name: "sync",
+  props: ["count"],
+  data() {
+      return {
+      }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
